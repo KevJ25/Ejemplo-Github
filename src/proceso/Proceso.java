db97509706281a3da91e6634067e52b42b11f47d
KevJ25 <medranohdzkevyn@gmail.com>
2024-10-02 10:42:05 -0600

    Subiendo Programa

---
+++ src/proceso/Proceso.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proceso;
+
+/**
+ *
+ * @author medra
+ */
+public class Proceso
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("""
+            1. Abrir IDE Netbeans\n                       
+            2. Crear proyecto nuevo\n
+            3. Nombrar proyecto\n
+            4. Crear paquete nuevo\n
+            5. Crear nueva clase\n
+            6. Nombrar clase\n
+            7. Escribir Codigo\n
+            8. Crear main\n
+            9. Ejecutar main\n
+            10. Abrir terminal\n
+                          """);
+
+    }
+
+}
---
+++ src/proceso/Proceso1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proceso;
+
+/**
+ *
+ * @author medra
+ */
+public class Proceso1
+{
+    String var = "Proceso 1";
+    
+}
